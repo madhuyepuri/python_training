e9382e8d4a268d4a2a2eddf6eb3a6ccae4df2bfb
MadhuYepuri <mayepuri@in.ibm.com>
2024-06-11 09:01:32 +0530

    my day-1 python code

---
+++ bin/1_doc_string_and_comments.py
@@ -0,0 +1,17 @@
+"""
+Provide description about the program here
+"""
+
+"""
+We can also Use this for
+multiline comment
+"""
+
+"""
+We can also Use this for
+multiline comment
+"""
+
+# Use this for single line comment
+
+print("hello")
